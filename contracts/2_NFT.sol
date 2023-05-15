fa121e6554f85c39da633690d10c655b0133d688
mohan.g <mohan.g@perpetualblock.io.com>
2023-05-15 14:41:49 +0530

    NFT code changes

--- contracts/2_NFT.sol
+++ contracts/2_NFT.sol
@@ -1,87 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-
-  struct NFTListing {
-        uint256 price;
-        address seller;
-    }
+contract MyToken is ERC721, Ownable {
+    constructor() ERC721("MOhanG", "MG") {}
+    using SafeMath for uint;
+ struct NFTOnwers{
+     address seller;
+     uint price;
+ }
+ uint public remaining;
+ address public mohanaddress;
+ mapping (uint => NFTOnwers[]) public buyers;
  
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+       // buyers[tokenId].push(NFTOnwers(msg.sender,price));
+    }
 
-contract NFTMarket is ERC721URIStorage,Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    using SafeMath for uint256;
-    address public _owner;
-    NFTListing[] public  sellers;
-    sellers.push(NFTListing.price);
-    sellers.push();
+    function ListNFT(uint tokenid,uint price) public {
+        require(price > 0, "NFTMarket: price should be not suffient");
+        transferFrom(msg.sender, address(this), tokenid);
+        buyers[tokenid].push(NFTOnwers(msg.sender,price));
+        mohanaddress=buyers[tokenid][buyers[tokenid].length-1].seller;
+    }
+
+    function buyNFT(uint tokenid) public payable {
+       uint len= buyers[tokenid].length;
+       require(len>0,"array should not be less");
+       require(buyers[tokenid][len-1].price >0 , "NFTMarket: token does not exist");
+       require(msg.value >= buyers[tokenid][len-1].price, "price is less");
+      
+       
+        uint ethPrice= uint(msg.value);
+        uint eth= ethPrice.mul(96).div(100);
+ 
+         uint balance = eth.mul(4).div(100);
+          remaining = balance.div(len);
+         (bool success,)= (buyers[tokenid][len-1].seller).call{value : eth}("");
+         require(success,"its not success");
+         uint total =0;
+            
+        //);
+        for (uint i = 0;i<len;i++){
+            total = total+remaining;
+            (bool suc,)= (buyers[tokenid][i].seller).call{value:remaining}("");
+            require(suc,"inside");
+        }
+        
+       ERC721(address(this)).transferFrom(address(this), msg.sender, tokenid);
+    }
+
+    function withdraw() public  {
+        uint amount = address(this).balance;
+        (bool ss,)=payable(msg.sender).call{value:amount}("");
+        require(ss,"its failed");
+    }
+    function getBalance() public  view returns(uint)   {
+        return address(this).balance;
+    }
+
+
     
-    event NFTTransfer(uint256 tokenid,address from ,address to ,string tokenuri,uint256 Price);
-
-    mapping(uint256 => NFTListing) private _listings;
-
-    constructor() ERC721("MohanG", "MG") {  }
-
-    function createNFT(string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        emit NFTTransfer( newItemId, address(0),msg.sender,tokenURI,0);
-        return newItemId;
-    }
-
-    // List NFT 
-
-    function listNFT(uint tokenID,uint Price) public {
-
-        require(Price > 0 ,"NFTMarket: price must be greater than 0");
-        transferFrom(msg.sender, address(this), tokenID);
-        _listings[tokenID]= NFTListing(Price,msg.sender);
-        emit NFTTransfer( tokenID, msg.sender,address(this),"",Price);
-
-    }
-
-    // buyNFT
-    function buyNFT(uint tokenId) public payable {
-        NFTListing memory listing= _listings[tokenId];
-        require(listing.price >0 , "NFTMarket: nft is not listed for sale");
-        require(msg.value == listing.price, "NFTMarket: incorrect price");
-        ERC721(address(this)).transferFrom(address(this), msg.sender, tokenId);
-        payable(listing.seller).transfer(listing.price.mul(95).div(100));
-        emit NFTTransfer(tokenId, address(this),msg.sender, "", listing.price);
-    }
-
-    //cancel Listing
-
-    function cancelListing(uint256 tokenID) public payable{
-        NFTListing memory listing = _listings[tokenID];
-        require(listing.price >0 , "NFTMarket: nft is not listed for sale");
-        require(listing.seller==msg.sender,"NFTMarket: you're not the owner");
-        ERC721(address(this)).transferFrom(address(this), msg.sender, tokenID);
-         emit NFTTransfer(tokenID, address(this),msg.sender, "", 0);
-        clearListing(tokenID);
-
-    }
-
-    function clearListing(uint256 tokenID) internal {
-         _listings[tokenID].price=0;
-         _listings[tokenID].seller=address(0);
-    } 
-
-    function withDrawFunds() public onlyOwner {
-        uint256 balance= address(this).balance;
-        require(balance> 0 ,"NFTMarket: balace is zero");
-        payable(msg.sender).transfer(balance);
-    }
-
 }
